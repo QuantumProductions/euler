b6cfc02c7a79233c6a912d021c59c74d88506a86
quantumpotato <quantumpotato@gmail.com>
2017-01-13 08:36:38 -0500

    Convert lines to array of strings

--- e67.py
+++ e67.py
@@ -4,7 +4,11 @@
 # initial_lines.strip("\n")
 
 with open('triangle.txt') as temp_file:
-  lines = [line.rstrip('\n') for line in temp_file]
+  lines = [line.rstrip('\n').split(" ") for line in temp_file]
+
+# with lines:
+#   formatted_lines = []
 
 print(lines)
 solution_numbers = []
+
